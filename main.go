047a0fe3f5e4b3d92671b5c986d7bbf08bb736b7
Jakub <work.jhanko@gmail.com>
2026-01-13 16:41:04 +0100

    Make all functionality work, create Dockerfile and docker-compose.yml for deployment

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/j-hanko/Go-Mongo-RESTApi/controllers"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
-	"net/http"
-	"github.com/j-hanko/Go-Mongo-RESTApi/controllers"
 )
 
 func main() {
 
 	router := httprouter.New()
 	userController := controllers.NewUserController(getSession())
-	router.GET("/user/:id"userController.GetUser)
-	router.POST("/user",userController.CreateUser)
+	router.GET("/user/:id", userController.GetUser)
+	router.POST("/user", userController.CreateUser)
 	router.DELETE("/user/:id", userController.DeleteUser)
 	http.ListenAndServe(":9000", router)
 }
 
 func getSession() *mgo.Session {
-	session, err := mgo.Dial("mongodb://localhost:27017")
+	session, err := mgo.Dial("mongodb://mongo:27017")
 	if err != nil {
 		panic(err)
 	}
